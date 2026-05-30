c10fc3d8a63f44660465227436abfabfe7a71e5d
AdityaPatil1601 <adityapatil111000>
2026-05-30 20:02:29 +0530

    Prepared for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -8,11 +9,16 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 dotenv.config();
 
 // ? Middleware - IN MIDDLEWARE CORS NEEDS TO BE FIRST BEFORE RATELIMIT
-app.use(cors({ origin: "http://localhost:5173" })); // ? It is used to control which frontend/client can access your backend API.
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" })); // ? It is used to control which frontend/client can access your backend API.
+}
+
 app.use(express.json()); // ? This middleware will parse JSON bodies(req.body)
 app.use(rateLimiter);
 
@@ -23,6 +29,13 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
